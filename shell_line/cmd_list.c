b44ab7a0c0725d8466f39bd2eadd98d6688fd905
starlovekaikai <101969013+starlovekaikai@users.noreply.github.com>
2022-12-20 22:22:42 +0800

    Update cmd_list.c

--- shell_line/cmd_list.c
+++ shell_line/cmd_list.c
@@ -16,8 +16,20 @@ std_cmdlist_t std_cmdline_list_new()
 //删除一个标准化命令行列表
 void std_cmdline_list_del(std_cmdlist_t std_cmdlist)
 {
+  glst_destroy(std_cmdlist);
 }
 //向命令行列表中添加新的标准化命令
-void std_cmdline_list_apd(std_cmdlist_t std_cmdlist)
+void std_cmdlist_append(std_cmdlist_t std_cmdlist, std_cmdlinec_t std_cmdline)
 {
+  glst_appendNode(std_cmdlist, std_cmdline);
 }
+ //获取命令行列表长度
+ int std_cmdlist_length(std_cmdlist_t std_cmdlist)
+ {
+  glst_len(std_cmdlist);
+ }
+ //获取命令行列表某一行的命令
+  std_cmdlinev_t std_cmdlist_getline(std_cmdlist_t std_cmdlist, int pos)
+ {
+//问一下博非大哥
+ }
