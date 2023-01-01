3553f9aaad2342cef6e34286c7bcf6ac761b8a56
BugBubbles <823091936@qq.com>
2023-01-01 13:09:19 +0800

    modified_by_cbf

--- baseLib/general_str.c
+++ baseLib/general_str.c
@@ -12,18 +12,16 @@ typedef unsigned int uint;
 typedef char byte;
 typedef struct _gstr_
 {
-  size_t uint_size; // 每个子块实际占用空间
-  size_t cap_size;  // 数据部分总共占据的内存空间
-  byte *buff;       // 每个子块是一串连续的内存空间，可以直接存一个字符串
+  size_t cap_size; // 数据部分总共占据的内存空间
+  byte *buff;      // 每个子块是一串连续的内存空间，可以直接存一个字符串
 } _gstr_;
 
-gstrv_t gstr_new(_size_t nodesize)
+gstrv_t gstr_new()
 {
-  // 默认只创建一个元素那个大的线性表
+  // 默认只创建0个元素那个大的线性表
   gstrv_t temp = (gstrv_t)malloc(sizeof(_gstr_));
-  temp->cap_size = nodesize;
-  temp->uint_size = nodesize;
-  temp->buff = (byte *)malloc(nodesize * sizeof(byte));
+  temp->cap_size = 0;
+  temp->buff = NULL;
   return temp;
 }
 gstrv_t gstr_new_byCStr(const char *s)
@@ -36,24 +34,21 @@ gstrv_t gstr_new_byCStr(const char *s)
   }
   gstrv_t temp = (gstrv_t)malloc(sizeof(_gstr_));
   temp->cap_size = i * sizeof(char);
-  temp->uint_size = sizeof(char);
   temp->buff = (byte *)malloc(temp->cap_size * sizeof(byte));
   memcpy(temp->buff, p, temp->cap_size);
   return temp;
 }
-gstrv_t gstr_new_bySize(_size_t nodesize, _size_t size)
+gstrv_t gstr_new_bySize(_size_t gstrsize)
 {
   gstrv_t temp = (gstrv_t)malloc(sizeof(_gstr_));
-  temp->cap_size = nodesize * size;
-  temp->uint_size = nodesize;
-  temp->buff = (byte *)malloc(nodesize * size * sizeof(byte));
+  temp->cap_size = gstrsize * sizeof(char);
+  temp->buff = (byte *)malloc(sizeof(char) * gstrsize * sizeof(byte));
   return temp;
 }
 void gstr_destroy(gstrv_t s)
 {
   free(s->buff);
   s->cap_size = 0;
-  s->uint_size = 0;
   free(s);
 }
 void gstr_import(gstrv_t this, const char *src)
@@ -66,7 +61,6 @@ void gstr_import(gstrv_t this, const char *src)
   }
   free(this->buff);
   this->cap_size = i * sizeof(char);
-  this->uint_size = sizeof(char);
   this->buff = (byte *)malloc(this->cap_size * sizeof(byte));
   memcpy(this->buff, p, this->cap_size);
 }
@@ -88,119 +82,92 @@ void gstr_import_byInt(gstrv_t this, int src)
 }
 int gstr_len(gstrc_t s)
 {
-  return (s->cap_size / s->uint_size);
+  return (s->cap_size / sizeof(char));
 }
 // 仅比较字典序
-#define _NOT_COMPARABLE_ -2
 int gstr_cmp(gstrc_t a, gstrc_t b)
 {
-  if (a->uint_size == b->uint_size)
-  {
-    return memcmp(a->buff, b->buff, a->cap_size);
-  }
-  else
-  {
-    return _NOT_COMPARABLE_;
-  }
+  return memcmp(a->buff, b->buff, a->cap_size);
 }
 #define NO_FIND -1
 int gstr_findChr(gstrc_t s, char c)
 {
-  if (s->uint_size == sizeof(char))
+  char *p = s->buff;
+  uint len = gstr_len(s);
+  uint i = 0;
+  while (i < len)
   {
-    char *p = s->buff;
-    uint len = gstr_len(s);
-    uint i = 0;
-    while (i < len)
+    if (*(p + i) == c)
     {
-      if (*(p + i) == c)
-      {
-        return i; // 有的，返回序列值
-      }
-      else
-      {
-        i++;
-      }
+      return i; // 有的，返回序列值
+    }
+    else
+    {
+      i++;
     }
   }
   return NO_FIND; // 未查到，返回-1;
 }
-int gstr_findStr(gstrc_t s, const char *sub)
+int gstr_findStr(gstrc_t s, gstrc_t sub)
 {
-  if (s->uint_size == sizeof(char))
+  char *p = s->buff;
+  uint len = gstr_len(s);
+  uint i = 0;
+  while (i < len)
   {
-    char *p = s->buff;
-    uint len = gstr_len(s);
-    uint i = 0;
-    while (i < len)
+    if (memcmp(p + i, sub->buff, sub->cap_size) == 0)
     {
-      if (memcmp(p + i, sub, sizeof(*sub) / sizeof(char)) == 0)
-      {
-        return i; // 有的，返回序列值
-      }
-      else
-      {
-        i++;
-      }
+      return i; // 有的，返回序列值
+    }
+    else
+    {
+      i++;
     }
   }
   return NO_FIND; // 未查到，返回-1;
 }
 void gstr_assign(gstrv_t d, gstrc_t s)
 {
-  memcpy(d->buff, s->buff, gstr_len(s));
+  int d_len = gstr_len(d), s_len = gstr_len(s);
+  if (d_len < s_len)
+  {
+    memcpy(d->buff, s->buff, d_len);
+  }
+  else
+  {
+    memcpy(d->buff, s->buff, s_len);
+  }
 }
 void gstr_appendStr(gstrv_t d, gstrc_t s)
 {
-  if (d->uint_size == s->uint_size)
-  {
-    realloc(d->buff, s->cap_size);
-    memcpy(d->buff + gstr_len(d), s->buff, d->cap_size);
-    d->cap_size += s->cap_size;
-  }
-  else
-  {
-    return;
-  }
+  realloc(d->buff, s->cap_size);
+  memcpy(d->buff + gstr_len(d), s->buff, d->cap_size);
+  d->cap_size += s->cap_size;
 }
 void gstr_appendChr(gstrv_t d, char c)
 {
-  if (d->uint_size == sizeof(char))
-  {
-    realloc(d->buff, 1 * sizeof(char));
-    memcpy(d->buff + gstr_len(d), &c, 1 * sizeof(char));
-    d->cap_size += 1 * sizeof(char);
-  }
-  else
-  {
-    return;
-  }
+  realloc(d->buff, 1 * sizeof(char));
+  memcpy(d->buff + gstr_len(d), &c, 1 * sizeof(char));
+  d->cap_size += 1 * sizeof(char);
 }
 void gstr_insertSub(gstrv_t d, gstrc_t s, int pos)
 {
-  if (d->uint_size == s->uint_size)
-  {
-    byte *temp = (byte *)malloc(d->cap_size + s->cap_size);
-    memcpy(temp, d->buff, pos * d->uint_size);
-    memcpy(temp + pos, s->buff, s->cap_size);
-    memcpy(temp + pos + gstr_len(s), d->buff + pos, (gstr_len(d) - pos - 1) * d->uint_size);
-    realloc(d->buff, s->cap_size);
-    d->cap_size += s->cap_size;
-    free(d->buff);
-    d->buff = temp;
-  }
-  else
-  {
-    return;
-  }
+  byte *temp = (byte *)malloc(d->cap_size + s->cap_size);
+  memcpy(temp, d->buff, pos * sizeof(char));
+  memcpy(temp + pos, s->buff, s->cap_size);
+  memcpy(temp + pos + gstr_len(s), d->buff + pos, (gstr_len(d) - pos - 1) * sizeof(char));
+  realloc(d->buff, s->cap_size);
+  d->cap_size += s->cap_size;
+  free(d->buff);
+  d->buff = temp;
 }
 void gstr_deleteSub(gstrv_t d, int pos, int len)
 {
   uint raw_len = gstr_len(d);
-  byte *temp = (byte *)malloc((raw_len - len) * d->uint_size);
-  memcpy(temp, d->buff, pos * d->uint_size);
-  memcpy(temp + pos, d->buff + pos + len - 1, (raw_len - len - pos) * d->uint_size);
-  d->cap_size -= len * d->uint_size;
+  byte *temp = (byte *)malloc((raw_len - len) * sizeof(char));
+  memcpy(temp, d->buff, pos * sizeof(char));
+  memcpy(temp + pos, d->buff + pos + len - 1, (raw_len - len - pos) * sizeof(char));
+  d->cap_size -= len * sizeof(char);
   free(d->buff);
   d->buff = temp;
 }
@@ -230,7 +197,7 @@ char gstr_getChr(gstrc_t s, int pos)
     return *(p + pos);
   }
 }
-void gstr_setSub(gstrc_t s, const char *sub, int pos)
+void gstr_setSub(gstrc_t s, gstrc_t sub, int pos)
 {
   char *p = s->buff;
   uint len = gstr_len(s);
@@ -240,10 +207,10 @@ void gstr_setSub(gstrc_t s, const char *sub, int pos)
   }
   else
   {
-    memcpy(p + pos, sub, sizeof(*sub));
+    memcpy(p + pos, sub->buff, sub->cap_size);
   }
 }
-void gstr_getSub(gstrc_t s, char *sub, int pos, _size_t size)
+void gstr_getSub(gstrc_t s, gstrc_t sub, int pos, _size_t size)
 {
   char *p = s->buff;
   uint len = gstr_len(s);
@@ -253,9 +220,225 @@ void gstr_getSub(gstrc_t s, char *sub, int pos, _size_t size)
   }
   else
   {
-    memcpy(p + pos, sub, size * sizeof(char));
+    memcpy(p + pos, sub->buff, sub->cap_size);
   }
 }
 int gstr_skipChr(gstrc_t s, int pos, char c)
 {
+  char *p = s->buff;
+  uint len = gstr_len(s);
+  if (pos < 0 || pos > len)
+  {
+    return NO_FIND;
+  }
+  else
+  {
+    uint i = pos;
+    while (i < len)
+    {
+      if (*(p + i) != c)
+      {
+        return i; // 有的，返回序列值
+      }
+      else
+      {
+        i++;
+      }
+    }
+    return NO_FIND;
+  }
+}
+int gstr_skipSpChr(gstrc_t s, int pos)
+{
+  char *p = s->buff;
+  uint len = gstr_len(s);
+  if (pos < 0 || pos > len)
+  {
+    return NO_FIND;
+  }
+  else
+  {
+    uint i = pos + 1;
+    while (i < len)
+    {
+      if (*(p + i) != *(p + pos))
+      {
+        return i; // 有的，返回序列值
+      }
+      else
+      {
+        i++;
+      }
+    }
+    return NO_FIND;
+  }
+}
+int gstr_findStrFrom(gstrc_t s, int pos, gstrc_t sub)
+{
+  char *p = s->buff;
+  uint len = gstr_len(s);
+  if (pos < 0 || pos > len)
+  {
+    return NO_FIND;
+  }
+  else
+  {
+    uint i = pos + 1;
+    while (i < len)
+    {
+      if (memcmp(p + pos, sub->buff, sub->cap_size) == 0)
+      {
+        return i; // 有的，返回序列值
+      }
+      else
+      {
+        i++;
+      }
+    }
+    return NO_FIND;
+  }
+}
+int gstr_skipSpFindChr(gstrc_t s, int pos, char c)
+{
+  char *p = s->buff;
+  uint len = gstr_len(s);
+  if (pos < 0 || pos > len)
+  {
+    return NO_FIND;
+  }
+  else
+  {
+    uint i = pos + 1;
+    while (i < len)
+    {
+      if (*(p + i) == c)
+      {
+        return i; // 有的，返回序列值
+      }
+      else
+      {
+        i++;
+      }
+    }
+    return NO_FIND;
+  }
+}
+int gstr_skipSpFindStr(gstrc_t s, int pos, gstrc_t sub)
+{
+  uint len = gstr_len(s);
+  if (pos < 0 || pos > len)
+  {
+    return NO_FIND;
+  }
+  else
+  {
+    uint i = pos + 1;
+    gstrv_t dst = gstr_new();
+    gstr_removeSpPos(s, pos);
+    char *p = s->buff;
+    while (i < len)
+    {
+      if (memcmp(p + pos, sub->buff, sub->cap_size) == 0)
+      {
+        return i; // 有的，返回序列值
+      }
+      else
+      {
+        i++;
+      }
+    }
+    return NO_FIND;
+  }
+}
+int gstr_replaceOneChr(gstrv_t dst, char raw_c, char new_c, int pos)
+{
+  char *src_p = dst->buff;
+  uint len = gstr_len(dst);
+  if (pos < 0 || pos > len)
+  {
+    return NO_FIND;
+  }
+  else
+  {
+    uint i = pos + 1;
+    while ((i = gstr_skipSpFindChr(dst, i, raw_c)) != NO_FIND)
+    {
+      *(src_p + i) = new_c;
+    }
+    return 0;
+  }
+}
+int gstr_replaceAllChr(gstrv_t dst, char raw_c, char new_c)
+{
+  char *src_p = dst->buff;
+  uint i = 0;
+  while ((i = gstr_skipSpFindChr(dst, i, raw_c)) != NO_FIND)
+  {
+    *(src_p + i) = new_c;
+  }
+  return 0;
+}
+int gstr_replaceOneSub(gstrv_t dst, gstrc_t raw_sub, gstrc_t new_sub, int pos)
+{
+  char *src_p = dst->buff;
+  uint len = gstr_len(dst);
+  if (pos < 0 || pos > len)
+  {
+    return NO_FIND;
+  }
+  else
+  {
+    uint i = pos + 1;
+    uint sub_len = raw_sub->cap_size;
+    while ((i = gstr_findStrFrom(dst, i, raw_sub->buff)) != NO_FIND)
+    {
+      memcpy(src_p + i, new_sub, sub_len);
+    }
+    return 0;
+  }
+}
+int gstr_replaceAllSub(gstrv_t dst, gstrc_t raw_sub, gstrc_t new_sub)
+{
+  char *src_p = dst->buff;
+  uint i = 0;
+  uint sub_len = raw_sub->cap_size;
+  while ((i = gstr_findStrFrom(dst, i, raw_sub->buff)) != NO_FIND)
+  {
+    memcpy(src_p + i, new_sub, sub_len);
+  }
+  return 0;
+}
+int gstr_removeSpPos(gstrv_t dst, int pos)
+{
+  char *src_p = dst->buff;
+  uint len = gstr_len(dst);
+  if (pos < 0 || pos > len)
+  {
+    return NO_FIND;
+  }
+  else
+  {
+    uint i = pos;
+    gstrv_t temp = gstr_new();
+    while (*(src_p + i) != ' ')
+    {
+      gstr_appendChr(dst, *(src_p + i));
+    }
+    gstr_destroy(dst);
+    dst = temp;
+    return 0;
+  }
+}
+int gstr_removeSp(gstrv_t dst)
+{
+  char *src_p = dst->buff;
+  uint i = 0;
+  gstrv_t temp = gstr_new();
+  while (*(src_p + i) != ' ')
+  {
+    gstr_appendChr(temp, *(src_p + i));
+  }
+  gstr_destroy(dst);
+  dst = temp;
+  return 0;
 }
--- baseLib/general_str.h
+++ baseLib/general_str.h
@@ -18,9 +18,9 @@ extern "C"
   typedef struct _gstr_ *gstrv_t;       // 变量指针
   typedef const struct _gstr_ *gstrc_t; // 常量指针
   // 构造/析构
-  gstrv_t gstr_new(_size_t nodesize);
+  gstrv_t gstr_new();
   gstrv_t gstr_new_byCStr(const char *s);
-  gstrv_t gstr_new_bySize(_size_t nodesize, _size_t size);
+  gstrv_t gstr_new_bySize(_size_t gstrsize);
   void gstr_destroy(gstrv_t s);
   // 导入/导出
   void gstr_import(gstrv_t this, const char *src);
@@ -30,7 +30,7 @@ extern "C"
   int gstr_len(gstrc_t s);
   int gstr_cmp(gstrc_t a, gstrc_t b);
   int gstr_findChr(gstrc_t s, char c);
-  int gstr_findStr(gstrc_t s, const char *sub);
+  int gstr_findStr(gstrc_t s, gstrc_t sub);
   // 写操作
   void gstr_assign(gstrv_t d, gstrc_t s);
   void gstr_appendStr(gstrv_t d, gstrc_t s);
@@ -41,27 +41,31 @@ extern "C"
   void gstr_setChr(gstrc_t s, char c, int pos);
   char gstr_getChr(gstrc_t s, int pos);
   // 字符串存取
-  void gstr_setSub(gstrc_t s, const char *sub, int pos);
-  void gstr_getSub(gstrc_t s, char *sub, int pos, _size_t size);
+  void gstr_setSub(gstrc_t s, gstrc_t sub, int pos);
+  void gstr_getSub(gstrc_t s, gstrc_t sub, int pos, _size_t size);
   // 语法操作
-  //返回自pos位置以后的字符c的最近一个位置
+  // 返回自pos位置以后的不是字符c的最近一个位置
   int gstr_skipChr(gstrc_t s, int pos, char c);
-  //返回自pos位置以后，pos位置的字符下一个位置
+  // 返回自pos位置以后，不是pos位置的字符下一个位置
   int gstr_skipSpChr(gstrc_t s, int pos);
-  // 从gstr里面查找自pos位置以后的子串sub
-  int gstr_findStrFrom(gstrc_t s, int pos, const char *sub);
-  // 从gstr里面查找自pos位置以后的字符c
+  // // 从gstr里面查找自pos位置以后的子串sub
+  int gstr_findStrFrom(gstrc_t s, int pos, gstrc_t sub);
+  // 从gstr里面查找自pos位置以后，排除空格的字符c
   int gstr_skipSpFindChr(gstrc_t s, int pos, char c);
   // 从gstr里面查找自pos位置以后的子串sub
-  int gstr_skipSpFindStr(gstrc_t s, int pos, const char *sub);
+  int gstr_skipSpFindStr(gstrc_t s, int pos, gstrc_t sub);
   // 从gstr里面查找并替换自pos位置以后的第一个字符c
-  int gstr_replaceOneSub(gstrv_t dst, gstrc_t src, char c, int pos);
+  int gstr_replaceOneChr(gstrv_t dst, char raw_c, char new_c, int pos);
   // 从gstr里面查找并替换全部字符c
-  int gstr_replaceAllSub(gstrv_t dst, gstrc_t src, char c);
+  int gstr_replaceAllChr(gstrv_t dst, char raw_c, char new_c);
   // 从gstr里面查找并替换自pos位置以后的第一个子gstr
-  int gstr_replaceOneSub(gstrv_t dst, gstrc_t src, gstrc_t part, int pos);
+  int gstr_replaceOneSub(gstrv_t dst, gstrc_t raw_sub, gstrc_t new_sub, int pos);
   // 从gstr里面查找并替换全部子gstr
-  int gstr_replaceAllSub(gstrv_t dst, gstrc_t src, gstrc_t part);
+  int gstr_replaceAllSub(gstrv_t dst, gstrc_t raw_sub, gstrc_t new_sub);
+  // 从gstr里面查找并替换自pos以后的全部空格
+  int gstr_removeSpPos(gstrv_t dst,int pos);
+  // 从gstr里面查找并替换全部空格
+  int gstr_removeSp(gstrv_t dst);
 #ifdef __cplusplus
 }
 #endif
